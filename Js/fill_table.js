60e21de33b9aa45be7e1ebdf6a71cb25c4dc2c25
KiwiDu <kiwidu@outlook.com>
2016-05-08 12:52:11 +0800

    update css

--- Js/fill_table.js
+++ Js/fill_table.js
@@ -2,7 +2,7 @@ function init() {
     console.log("beg");
     createTable();
     fillTable();
-    activateTable
+    activateTable();
     console.log("end");
 }
 
@@ -22,40 +22,40 @@ function createTable() {
 
 function fillTable() {
     var elementlist = eval("(" + ajax("GET", "Js/info.json") + ")");
-    //var location = "nxxxxxxxxxxxxxxxxnmmxxxxxxxxxxnnnnnnmmxxxxxxxxxxmnnnnnmmppppppppppmmnnnnmmppppppppppmmmnnnmmppppppppppmmmmnnmmppppppppppxxxxxx";
     var location = ajax("GET", "locations.txt");
     var regexp = /[\n\r\f\t]/g;
     location = location.replace(regexp, '');
+    //get the location of each element 
+    //and delete the blanks & new-lines to make it easier to edit
 
-    var k = 0;
+    var k = 0;//the pointer points to the table cell
     for (var row = 0; row < 7; row++) {
-        //var trow = document.getElementById("tr-" + row.toString());
-
+        //row by row
         for (var cell = 18 * row; cell < (18 * (row + 1)); cell++) {
-            //var tcell = document.querySelector("td[no =" + "\"" + (cell + 1).toString() + "\"" + "]"); //TOFUNC
-            var tcell = select("td", "*", "*", "no", "\"" + (cell + 1).toString() + "\"");
-            //console.log("td[no =" + "\"" + (cell + 1).toString() + "\"" + "]");
-            if (elementlist[k]) {
-                if (location[cell] == 'x') {
+            //var tcell = document.querySelector("td[no =" + "\"" + (cell + 1).toString() + "\"" + "]"); //TOFUNC (DONE)
+            var tcell = select("td", "*", "*", "no", "\"" + (cell + 1).toString() + "\"");//select such a cell
+            if (elementlist[k]) {//are there such an element?
+                if (location[cell] == 'x') {//
                     tcell.style.backgroundColor = "transparent";
                     continue;
                 } else {
                     tcell.innerHTML = "<span>" + elementlist[k].name + "</span>" + "<span>" + elementlist[k].chname + "</span>";
                     switch (location[cell]) {
-                        case 'n':
+                        case 'n'://non-metal
                             tcell.style.backgroundColor = "#2196F3";
                             break;
-                        case 'm':
+                        case 'm'://metal
                             tcell.style.backgroundColor = "#009688";
                             break;
-                        case 'p':
+                        case 'p'://transtion metal
                             tcell.style.backgroundColor = "#8BC34A";
                             break;
+                        //TODO:MORE SECTIONS
                     }
-                    k++;
+                    k++;//move to the next table cell;
                 }
             } else {
-                tcell.style.backgroundColor = "transparent";
+                tcell.style.backgroundColor = "transparent";//empty cell
                 continue;
             }
         }
@@ -64,7 +64,7 @@ function fillTable() {
 
 function activateTable() {
 
-}
+}//TODO
 
 function newblock(ele, idn, classn, no) {
     var e = document.createElement(ele);
